db44b13b8a62243810fbec710b5c8c4d13e8e07f
VCH <v.chiriliuc@gmail.com>
2023-11-01 20:58:55 +0100

    Add PokemonRepositoryTests

---
+++ src/test/java/com/pokemonreview/api/repository/PokemonRepositoryTests.java
@@ -0,0 +1,28 @@
+package com.pokemonreview.api.repository;
+
+import com.pokemonreview.api.models.Pokemon;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
+public class PokemonRepositoryTests {
+
+    @Autowired
+    private PokemonRepository pokemonRepository;
+
+    @Test
+    public void PokemonRepository_SaveAll_ReturnSavedPokemon(){
+        //Arrange
+        Pokemon pokemon = Pokemon.builder().name("Picachu").type("Electric").build();
+        //Act
+        Pokemon savedPokemon = pokemonRepository.save(pokemon);
+        //Assert
+        //Assertions.assertThat(savedPokemon).isNotNull();
+        Assertions.assertEquals(pokemon,savedPokemon);
+    }
+}
