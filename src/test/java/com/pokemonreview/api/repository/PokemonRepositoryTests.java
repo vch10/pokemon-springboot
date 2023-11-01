b939fd806da4564f13c43499a07007156d0fb82a
VCH <v.chiriliuc@gmail.com>
2023-11-01 21:28:11 +0100

    Add 2 tests to PokemonRepositoryTests

--- src/test/java/com/pokemonreview/api/repository/PokemonRepositoryTests.java
+++ src/test/java/com/pokemonreview/api/repository/PokemonRepositoryTests.java
@@ -8,6 +8,8 @@ import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import java.util.List;
+
 @DataJpaTest
 @AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
 public class PokemonRepositoryTests {
@@ -24,5 +26,34 @@ public class PokemonRepositoryTests {
         //Assert
         //Assertions.assertThat(savedPokemon).isNotNull();
         Assertions.assertEquals(pokemon,savedPokemon);
+        Assertions.assertTrue(savedPokemon.getId() > 0);
+    }
+
+    @Test
+    public void PokemonRepository_GetAll_ReturnMoreThanOnePokemon(){
+        //Arrange
+        Pokemon pokemon = Pokemon.builder().name("Picachu").type("Electric").build();
+        Pokemon pokemon2 = Pokemon.builder().name("Picachu2").type("Electric").build();
+
+        //Act
+        pokemonRepository.save(pokemon);
+        pokemonRepository.save(pokemon2);
+        List<Pokemon> pokemonList = pokemonRepository.findAll();
+
+        //Assert
+        Assertions.assertEquals(pokemonList.size(),2);
+    }
+
+    @Test
+    public void PokemonRepository_FindById_ReturnPokemon(){
+        //Arrange
+        Pokemon pokemon = Pokemon.builder().name("Picachu").type("Electric").build();
+
+        //Act
+        pokemonRepository.save(pokemon);
+        Pokemon findedPokemon = pokemonRepository.findById(pokemon.getId()).get();
+
+        //Assert
+        Assertions.assertEquals(findedPokemon, pokemon);
     }
 }
